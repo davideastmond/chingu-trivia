9899586d6a3e49e33c7317ab69d95e5719450c84
davideastmond <davebuck1981@gmail.com>
2020-10-07 10:22:01 -0400

    Updates

---
+++ precache-manifest.b2f976e4b8eb59c9f3d0274033ce2846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f363e23e8cc3eb0f5f0d1199f9ed8b97",
+    "url": "/chingu-trivia/index.html"
+  },
+  {
+    "revision": "86ee86d60e4a850ee940",
+    "url": "/chingu-trivia/static/css/main.8b3b4765.chunk.css"
+  },
+  {
+    "revision": "19c53b28c313b562341b",
+    "url": "/chingu-trivia/static/js/2.f91fb91d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chingu-trivia/static/js/2.f91fb91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ee86d60e4a850ee940",
+    "url": "/chingu-trivia/static/js/main.34efc8a6.chunk.js"
+  },
+  {
+    "revision": "e1ad8f1f6e5d0a916da1",
+    "url": "/chingu-trivia/static/js/runtime-main.97ec3d99.js"
+  }
+]);
